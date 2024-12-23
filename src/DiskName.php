9336b0214928fcc5acb3e140a74a4b4d148b745a
John Robinson <john@bayfrontmedia.com>
2024-12-23 15:03:24 -0500

    Typed constant bugfix

--- src/DiskName.php
+++ src/DiskName.php
@@ -9,17 +9,17 @@ class DiskName
      * Names of official Flysystem adapters
      */
 
-    public const string LOCAL = 'LOCAL';
-    public const string FTP = 'FTP';
-    public const string MEMORY = 'MEMORY';
-    public const string PATH_PREFIXING = 'PATH_PREFIXING';
-    public const string AWS_S3 = 'AWS_S3';
-    public const string ASYNC_AWS_S3 = 'ASYNC_AWS_S3';
-    public const string AZURE_BLOB_STORAGE = 'AZURE_BLOB_STORAGE';
-    public const string GOOGLE_CLOUD_STORAGE = 'GOOGLE_CLOUD_STORAGE';
-    public const string SFTP_V2 = 'SFTP_V2';
-    public const string SFTP_V3 = 'SFTP_V3';
-    public const string WEBDAV = 'WEBDAV';
-    public const string ZIP_ARCHIVE = 'ZIP_ARCHIVE';
+    public const LOCAL = 'LOCAL';
+    public const FTP = 'FTP';
+    public const MEMORY = 'MEMORY';
+    public const PATH_PREFIXING = 'PATH_PREFIXING';
+    public const AWS_S3 = 'AWS_S3';
+    public const ASYNC_AWS_S3 = 'ASYNC_AWS_S3';
+    public const AZURE_BLOB_STORAGE = 'AZURE_BLOB_STORAGE';
+    public const GOOGLE_CLOUD_STORAGE = 'GOOGLE_CLOUD_STORAGE';
+    public const SFTP_V2 = 'SFTP_V2';
+    public const SFTP_V3 = 'SFTP_V3';
+    public const WEBDAV = 'WEBDAV';
+    public const ZIP_ARCHIVE = 'ZIP_ARCHIVE';
 
 }
